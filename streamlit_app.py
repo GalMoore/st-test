d5effc1f09f81ada1a5744913f09297705efc27c
Gal Moore <gal@galmoore.co.uk>
2023-06-29 15:47:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,11 @@ try:
     # Attempt to import langchain
     import langchain
     print("langchain is installed.")
+    st.write("langchain is installed.")
     print("Version:", langchain.__version__)
 except ImportError:
     print("langchain is not installed.")
-  
+    st.write("langchain is NOT installed.")
+
 st.title("Hello world")
 st.balloons()
